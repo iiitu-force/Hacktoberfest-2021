2c7915b441066cfe49a9b222b8559f1f29373114
Shridhar Chouksey <shridharchouksey100@gmail.com>
2021-10-25 16:28:03 +0530

    Binary Converter.java

---
+++ Binary Converter.java
@@ -0,0 +1,42 @@
+//Binary to Decimal Converter
+
+public class Main {
+    
+    public static int BinaryToDecimal(int binary){
+    
+        int decimal = 0;
+    
+        int n = 0;
+    
+        while(true)
+        {
+        
+            if(binary == 0){
+                break;
+            } 
+            else {
+    
+                int temp = binary%10;
+    
+                decimal += temp*Math.pow(2, n);
+    
+                binary = binary/10;
+    
+                n++;
+    
+            }
+    
+        }
+    
+        return decimal;
+    }
+    
+    public static void main(String[] args) {
+       
+        System.out.println("Decimal of 1101 is: "+BinaryToDecimal(1101));
+       
+        System.out.println("Decimal of 10101 is: "+BinaryToDecimal(10101));
+       
+        System.out.println("Decimal of 11111 is: "+BinaryToDecimal(11111));
+    }
+}
